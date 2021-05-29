018d05b5b42f5a59c042578461b40cfefd0bbdf3
Ibaev Danil <danil.ibaev@yandex.ru>
2021-05-29 13:32:59 +0300

    done

--- src/itis/q32/MainClass.java
+++ src/itis/q32/MainClass.java
@@ -1,6 +1,5 @@
 package itis.q32;
 
-
 /*
 В папке resources находятся два .csv файла.
 Один содержит данные о группах в университете в следующем формате: ID группы, название группы, код группы
@@ -14,12 +13,23 @@ package itis.q32;
 2. Самую маленькую группу
 3. Отношение группа - сумма балов студентов фамилия которых совпадает с заданной строкой
 4. Отношения студент - дельта баллов до проходного порога (порог передается параметром),
- сгруппированные по признаку пройден порог, или нет
+сгруппированные по признаку пройден порог, или нет
 
 Требования к реализации: все методы в StudentService должны быть реализованы с использованием StreamApi.
 Использование обычных циклов и дополнительных переменных приведет к снижению баллов, но допустимо.
 Парсинг файлов и реализация методов оцениваются ОТДЕЛЬНО
 */
+
+import itis.q32.entities.Group;
+import itis.q32.entities.Student;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainClass {
 
@@ -27,21 +37,39 @@ public class MainClass {
 
     public static void main(String[] args) {
         new MainClass().run(
-                "",
-                "");
+                "C:\\prgmm\\src\\q32-students\\src\\itis\\q32\\resources\\students.csv",
+                "C:\\prgmm\\src\\q32-students\\src\\itis\\q32\\resources\\groups.csv");
     }
 
     private void run(String studentsPath, String groupsPath) {
+        try {
+            ArrayList<Student> students = new ArrayList<>();
+            HashMap<String, Group> groups = new HashMap<>();
+            BufferedReader br = new BufferedReader(new FileReader(groupsPath));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] arr = line.split(",");
+                groups.put(arr[0],
+                        new Group(arr[1].trim(), Long.parseLong(arr[0].trim()), arr[2].trim()));
+            }
+
+            br = new BufferedReader(new FileReader(studentsPath));
+            while((line = br.readLine())!=null){
+                String[] arr = line.split(",");
+                students.add(
+                        new Student(arr[0].trim(),groups.get(arr[2].trim()),Integer.parseInt(arr[3].trim()), LocalDate.parse(arr[1].trim())));
+            }
+
+// tests
+// count
+            System.out.println(studentService.countGroupsWithOnlyAdultStudents(students));
+// surname
+            System.out.println(studentService.getGroupScoreSumMap(students,"Смит"));
+            System.out.println(studentService.getSmallestGroup(students));
+            System.out.println(studentService.groupStudentScoreWithThreshold(students,50));
+        } catch (IOException e) {
+
+        }
     }
 
 }
-
-
-
-
-
-
-
-
-
-
--- src/itis/q32/StudentServiceImpl.java
+++ src/itis/q32/StudentServiceImpl.java
@@ -3,29 +3,48 @@ package itis.q32;
 import itis.q32.entities.Group;
 import itis.q32.entities.Student;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StudentServiceImpl implements StudentService {
 
-
     @Override
     public Group getSmallestGroup(List<Student> students) {
-        return null;
+        return students.stream()
+                .collect(Collectors.toMap(Student::getGroup, student -> new ArrayList<Student>(){{add(student);}},
+                        (oldStudentList, newStudentList) -> {
+                            oldStudentList.addAll(newStudentList);
+                            return oldStudentList;
+                        }))
+                .entrySet().stream().max(Comparator.comparingInt(e -> e.getValue().size())).get().getKey();
     }
 
     @Override
     public Integer countGroupsWithOnlyAdultStudents(List<Student> students) {
-        return null;
+        return Math.toIntExact(
+                students.stream().collect(Collectors.groupingBy(Student::getGroup,Collectors.toList()))
+                        .entrySet().stream()
+                        .filter(e->e.getValue().size()!=0 && e.getValue().size()
+                                == e.getValue().stream().filter(s->LocalDate.from(s.getBirthdayDate()).getYear()>18)
+                                .count()).count());
     }
 
     @Override
     public Map<String, Integer> getGroupScoreSumMap(List<Student> students, String studentSurname) {
-        return null;
+        return students.stream()
+                .filter(student -> student.getFullName().split(" ")[0].equals(studentSurname))
+                .collect(Collectors.toMap(student -> student.getGroup().getTitle(),student->1, Integer::sum));
     }
 
     @Override
     public Map<Boolean, Map<String, Integer>> groupStudentScoreWithThreshold(List<Student> students, Integer threshold) {
-        return null;
+        return students.stream()
+                .collect(
+                        Collectors.groupingBy(student -> student.getScore() > threshold,
+                                Collectors.toMap(Student::getFullName,Student::getScore)));
     }
 }
